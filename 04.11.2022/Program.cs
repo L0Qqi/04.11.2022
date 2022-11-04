742bbe5e469be76ffe8a675c49cfe4378539034f
loqi <loqi@DESKTOP-BT5NG3V>
2022-11-04 23:52:18 +0300

    Добавьте файлы проекта.

---
+++ 04.11.2022/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    //главный
+    public class Борис { }
+
+    //финансовый директор(Рашид)
+    class Рашид : Борис { } 
+    class Лукас : Рашид { }
+    class Бухгалтерия : Лукас { }
+
+    // директор по автоматизации:
+    class Оильхам : Борис { } 
+    class Оркадий : Оильхам { }
+    class Володя : Оркадий { }
+
+    // системщики
+    class Ильшат : Володя { } 
+    class Иваныч : Ильшат { }
+    class Илья : Иваныч { }
+    class Витя : Иваныч { }
+    class Женя : Иваныч { }
+    
+    // разработчики
+    class Сергей : Володя { }
+    class Ляйсан : Сергей { }
+    class Марат : Ляйсан { }
+    class Дина : Ляйсан { }
+    class Ильдар : Ляйсан { }
+    class Антон : Ляйсан { }
+    internal class Program
+    {
+        static void Otvet(Type a, Type b)
+        {
+            if (a.IsSubclassOf(b)) { Console.WriteLine("Да, господин"); } // проверяем является ли классы наследниками друг друга
+            else { Console.WriteLine("Нет, пошёл ты"); }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите от кого хотите дать задачу");
+            string A1 = Console.ReadLine();
+            Console.WriteLine("Введите кому хотите дать задачу");
+            string B1 = Console.ReadLine();
+            Console.WriteLine("Введите название задачи");
+            string C1 = Console.ReadLine();
+            Type A = Type.GetType("HW" + "." + A1, false, true); // получаем типы классов по имени
+            Type B = Type.GetType("HW" + "." + B1, false, true);
+            Console.WriteLine($"Итак, от {A1} дана задача {C1}  для {B1}.");
+            Console.WriteLine("Ответ сотрудника:");
+            Otvet(B, A);
+        }
+    }
+}
---
+++ 04.11.2022/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("04.11.2022")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04.11.2022")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4d36796f-1955-4aea-b673-1211d019cf3d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
